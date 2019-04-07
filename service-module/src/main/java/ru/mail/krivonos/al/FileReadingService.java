110118964d1c5c9baa76ab30a8f23b15d3051995
krivonos-alex <krivonos-al@mail.ru>
2019-04-07 15:54:41 +0300

    Add all service-module services with needed exceptions and constants

---
+++ service-module/src/main/java/ru/mail/krivonos/al/FileReadingService.java
@@ -0,0 +1,6 @@
+package ru.mail.krivonos.al;
+
+public interface FileReadingService {
+
+    String readFile(String fileName);
+}
---
+++ service-module/src/main/java/ru/mail/krivonos/al/impl/FileReadingServiceImpl.java
@@ -0,0 +1,26 @@
+package ru.mail.krivonos.al.impl;
+
+import ru.mail.krivonos.al.FileReadingService;
+import ru.mail.krivonos.al.FileRepository;
+import ru.mail.krivonos.al.constants.ServiceConstants;
+
+import java.util.List;
+
+public class FileReadingServiceImpl implements FileReadingService {
+
+    private FileRepository fileRepository;
+
+    //Public constructor with arguments for better testability
+    public FileReadingServiceImpl(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
+    @Override
+    public String readFile(String fileName) {
+        List<String> lines = fileRepository.readFile(fileName);
+        return lines.stream()
+                .filter(line -> line.matches(ServiceConstants.NUMBERS_STRING_REGEX))
+                .findAny()
+                .orElse("");
+    }
+}
